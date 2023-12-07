b5e2167de556c0707da5e5c7380773e69afefc8c
Likith <lpalabin@ucsd.edu>
2023-12-07 13:38:25 -0800

    wrote training routine

--- train.py
+++ train.py
@@ -2,12 +2,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 from DataLoader import DataBatch, concatenate_data
-from Transformer import Encoder, Decoder
+from Transformer import AutoEncoder
 import os
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
-import tqdm
+from tqdm import tqdm
 
 device = (
     "cuda"
@@ -23,26 +23,28 @@ data_dir = 'cifar-10-batches-py'
 
 data, labels = concatenate_data(data_dir)
 data = np.transpose(data, (0, 3, 1, 2)).astype(np.float64)
+data = data/255.0
 
 with open(os.path.join(data_dir, 'batches.meta'), 'rb') as f:
     meta_data = pickle.load(f, encoding='bytes')
     label_names = meta_data[b'label_names']
 
-encoder = Encoder()
-encoder = encoder.double()
 
-decoder = Decoder()
-decoder = decoder.double()
+ae = AutoEncoder()
+ae = ae.double()
+ae.to(device)
 
-out = encoder.forward(torch.from_numpy(data[0]))
-out = decoder.forward(out)
-print(out.shape)
-# optimizer = torch.optim.Adam(encoder.parameters(), lr=1e-3, weight_decay=1e-6)
-# loss_fn = nn.MSELoss()
+optimizer = torch.optim.Adam(ae.parameters(), lr=1e-3, weight_decay=1e-6)
+loss_fn = nn.MSELoss()
 
-# batchsize = 64
-
-# for train_x, train_y in tqdm(DataBatch(data, labels, batchsize), total=len(data)//batchsize):
-#     optimizer.zero_grad()
-#     output = model.forward(train_x)
-#     loss = loss_fn(output, )
+batchsize = 128
+epochs = 5
+for epoch in range(epochs):
+    for train_x, _ in tqdm(DataBatch(data, labels, batchsize), total=len(data)//batchsize):
+        train_x = train_x.to(device)
+        optimizer.zero_grad()
+        output = ae.forward(train_x)
+        loss = loss_fn(output, train_x)
+        loss.backward()
+        optimizer.step()
+    print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, epochs, loss.item()))
