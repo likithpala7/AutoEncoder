d396b0a9c2866ec55f6eff0217d362c4e8123af9
Likith <lpalabin@ucsd.edu>
2023-12-07 13:37:57 -0800

    created autoencoder class

--- Transformer.py
+++ Transformer.py
@@ -9,15 +9,13 @@ class Encoder(nn.Module):
         self.model = nn.Sequential(
             nn.Conv2d(3, 64, (3, 3), padding="same"),
             nn.ReLU(),
-            nn.MaxPool2d((2, 2), padding=1),
+            nn.MaxPool2d((2, 2)),
             nn.Conv2d(64, 64, (3, 3), padding="same"),
             nn.ReLU(),
-            nn.MaxPool2d((2, 2), padding=1),
+            nn.MaxPool2d((2, 2)),
             nn.Conv2d(64, 32, (3, 3), padding="same"),
             nn.ReLU(),
-            nn.MaxPool2d((2, 2), padding=1),
-            nn.Flatten(0),
-            nn.Linear(32*5*5, 512)
+            nn.MaxPool2d((2, 2))
         )
 
     def forward(self, x):
@@ -29,19 +27,29 @@ class Decoder(nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.model = nn.Sequential(
-            nn.Linear(512, 2048),
-            nn.Unflatten(1, (8, 8, 32)),
-            nn.Conv2d(32, 32, (3, 3), padding='same'),
+            nn.ConvTranspose2d(32, 32, (3, 3), padding=1),
             nn.ReLU(),
             nn.Upsample(scale_factor=(2, 2)),
-            nn.Conv2d(32, 64, (3, 3), padding='same'),
+            nn.ConvTranspose2d(32, 64, (3, 3), padding=1),
             nn.ReLU(),
             nn.Upsample(scale_factor=(2, 2)),
-            nn.Conv2d(64, 64, (3, 3), padding='same'),
+            nn.ConvTranspose2d(64, 64, (3, 3), padding=1),
             nn.ReLU(),
             nn.Upsample(scale_factor=(2, 2)),
-            nn.Conv2d(64, 3, (3, 3), padding='same'),
+            nn.ConvTranspose2d(64, 3, (3, 3), padding=1),
             nn.ReLU()
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+    
+class AutoEncoder(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.model = nn.Sequential(
+            Encoder(),
+            Decoder()
         )
 
     def forward(self, x):
